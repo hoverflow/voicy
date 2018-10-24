217981fc150a327ccf755c3ab6f136e38cfa72fa
Jonathan <jonathan.stevano@ellysse.it>
2018-10-24 11:58:53 +0200

    added body-parser, added static file handler to serve assets and static

--- app.js
+++ app.js
@@ -1,35 +1,69 @@
 var config = require('./config.json');
-var express = require('express'); 
+var express = require('express');
 var session = require('express-session');
+var bodyparser = require('body-parser');
+
+var staticFileHandler = express.static(__dirname);
 
 /* modules */
 var authentication = require('./modules/authentication.js');
 
 /* initialize express app*/
-var app = new express();  
+var app = new express();
 
 /* initialize session */
 app.set('trust proxy', 1);
 app.use(session({
-  secret: 'Secr3t18',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: 'Secr3t18',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false
+    }
 }));
 
-/* serve static asset files directory: assets */
-app.use(express.static('assets'));
+/* initialize body parser for post and JSON data */
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
 
 /* route to authentication module */
-app.get('/authentication/:function',function (req, res) {
-    console.log('sid',req.session.id);
-    authentication.route(req,res);
+app.all('/authentication/:function', function (req, res) {
+    console.log('sid', req.session.id);
+    authentication.route(req, res);
 });
 
+app.get('/assets/*', function(req, res, next){
+    console.log('request asset',req.url);
+    if(req.session.auth)
+    {
+        staticFileHandler(req,res,next);        
+    }
+    else
+    {
+        res.status(200).send('yes');
+    }
+});
 
+app.get('/public/*', function(req, res, next){
+    staticFileHandler(req,res,next);
+});
 
-app.listen(config.http_listener, function(err) {  
-    if (typeof(err) == "undefined") {  
-        console.log('application running on: ' + config.http_listener);  //debugging purpose only
-    }  
-}); 
+/* default page handler (if not specified)
+   if user is authenticated the default page is the web application page,
+   otherwise will be the login page
+*/
+app.get('/', function (req, res) {
+    //check authentication
+    if (req.session.auth) {
+        res.redirect('/assets/index.html?ts='+ (new Date().getTime()));
+    } else {
+        res.redirect('/public/index.html');
+    }
+});
+
+app.listen(config.http_listener, function (err) {
+    if (typeof (err) == "undefined") {
+        console.log('application running on: ' + config.http_listener); //debugging purpose only
+    }
+});
